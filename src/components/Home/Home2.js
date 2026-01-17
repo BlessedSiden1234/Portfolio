ea3e6a88596f7ba0ced27ee90c1643be3c8c592a
Blessed Siden <kennethsiden@gmail.com>
2026-01-16 18:56:25 -0600

    updated unused variables to prevent crash in vercel

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -4,8 +4,6 @@ import myImg from "../../Assets/user-1.png";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
